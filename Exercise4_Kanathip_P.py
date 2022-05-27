f23bf5aa9363c49b4f62f95e263857c3873cdda1
mrKana6789 <106327440+mrKana6789@users.noreply.github.com>
2022-05-27 10:11:24 +0700

    homework

---
+++ Exercise4_Kanathip_P.py
@@ -0,0 +1,12 @@
+"""ตัวแปร"""
+foundationEnglish = 72
+generalBusiness = 65
+introductionToComputerSystems = 73
+computerProgramming = 81
+"""แสดงผลข้อมูล"""
+print("------------- Your Score -------------")
+print("Foundation English               :",foundationEnglish)
+print("General Business                 :",generalBusiness)
+print("Introduction To Computer Systems :",introductionToComputerSystems)
+print("Computer Proramming              :",computerProgramming)
+print("--------------------------------------")
