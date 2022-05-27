01126d4ba1e9a526e918d2fffba14d87cefbbbb9
mrKana6789 <106327440+mrKana6789@users.noreply.github.com>
2022-05-27 13:01:51 +0700

    Exercise 5

---
+++ Exercise5_1_Kanathip_P.py
@@ -0,0 +1,8 @@
+'''input'''
+a = float(input("a="))
+b = float(input("b="))
+'''output'''
+print("a+b =",a+b)
+print("a-b =",a-b)
+print("a*b =",a*b)
+print("a/b =",a/b)
---
+++ Exercise5_2_Kanathip_P.py
@@ -0,0 +1,6 @@
+print("Distance Calculator")
+'''input'''
+v = float(input("input velocity(m/s):"))
+t = float(input("input time(s):"))
+'''output'''
+print(v*t,"km/h")
